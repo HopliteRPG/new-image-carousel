07c42feb92ede0a45e6b881f203b41fa5a804122
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-05-27 13:56:43 -0400

    basic display image

---
+++ src/carouselcode-folder/carousel.js
@@ -0,0 +1,14 @@
+export { displayImage };
+function displayImage(startingPoint, array) {
+  let displayImageCounter = startingPoint;
+
+  if (displayImageCounter == array.length) {
+    displayImageCounter = 0;
+    console.log(array[displayImageCounter]);
+  } else if (displayImageCounter == -1) {
+    displayImageCounter = array.length - 1;
+    console.log(array[displayImageCounter]);
+  } else {
+    console.log(array[displayImageCounter]);
+  }
+}
